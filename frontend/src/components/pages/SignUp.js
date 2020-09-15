7247a9f4ec5c1953630e4b1fb3ca8801b2722959
CJHJ <corronade@gmail.com>
2020-09-15 15:23:29 +0900

    Center main container elements

--- frontend/src/components/pages/SignUp.js
+++ frontend/src/components/pages/SignUp.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 export default class SignUp extends Component {
   render() {
-    return (
-      <div>
-        <h1>Sign Up</h1>
-      </div>
-    );
+    return <h1>Sign Up</h1>;
   }
 }
