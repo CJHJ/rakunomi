7d7a08d27a781c0766474c6654af59926c7c434b
CJHJ <corronade@gmail.com>
2020-09-16 09:36:33 +0900

    Add bootstrap to page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import Header from "./components/Header";
