931d604e63a8265a6e6bc1c0d4f8f8a8a5b21e79
CJHJ <corronade@gmail.com>
2020-09-16 09:50:53 +0900

    Add authentication service

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import API_SIGNUP_URL from "../constants";
+
+const signup = (username, email, password, rakutenId, zoomId) => {
+  return axios.post(API_SIGNUP_URL, {
+    username,
+    email,
+    password,
+    rakutenId,
+    zoomId,
+  });
+};
+
+const login = (username, password) => {
+  return axios
+    .post(API_SIGNIN_URL, {
+      username,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+export default {
+  signup,
+  login,
+  logout,
+  getCurrentUser,
+};
