3d1c187017e4e00bc617df08fe2c86e644430f17
CJHJ <corronade@gmail.com>
2020-09-16 13:38:00 +0900

    Change port number

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api/";
+const API_URL = "http://localhost:5000/api/";
 const API_SIGNUP_URL = API_URL + "signup";
 const API_SIGNIN_URL = API_URL + "signin";
 
