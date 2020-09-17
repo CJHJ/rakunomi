7613094a3b8edafc149aa10fb4b46dd17658a1b4
kazu <nkz0914ssk@gmail.com>
2020-09-17 18:56:29 +0900

    Added Icons to meeting list

---
+++ frontend/src/components/atoms/Icons/Participated.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Emoji from "./Emoji";
+
+export default function Participated() {
+  return <Emoji symbol={"0x1F551"} />;
+}
---
+++ frontend/src/components/atoms/Icons/index.js
@@ -0,0 +1,6 @@
+export { default as Emoji } from "./Emoji";
+export { default as Beer } from "./Beer";
+export { default as ClinkingBeer } from "./ClinkingBeer";
+export { default as Invited } from "./Invited";
+export { default as Participated } from "./Participated";
+export { default as All } from "./All";
