4eab61939231eac96d52dfc5c5332afaf720535b
Jinge Wang <ar-jinge.wang@rakuten.com>
2020-09-17 10:25:23 +0900

    remove page in server api

--- backend/app/meeting/routes.py
+++ backend/app/meeting/routes.py
@@ -8,64 +8,79 @@ from datetime import datetime
 @bp.route('/meetings', methods=['GET'])
 @jwt_required
 def get_confirmed_list():
-    page = request.args.get('page', 1, type=int)
     user_id = request.args.get('user_id', type=int)
     cur_user = Users.query.get(user_id)
     if not cur_user:
         return jsonify({"msg": "Failed to find this user", "data": ""}), 401
-    meetings = cur_user.confirmed_meetings().paginate(page, app.config['PER_PAGE'], False)
-    ret = {'msg': 'Success',
-           'data': [{'meeting_id': me.id,
-                     'meeting_name': me.name,
-                     'leader_user_id': me.leader_id,
-                     'Datetime': me.datetime} for me in meetings.iter_pages()]}
+    meetings = cur_user.confirmed_meetings().all()
+    ret = {
+        'msg':
+            'Success',
+        'data': [{
+            'meeting_id': me.id,
+            'meeting_name': me.name,
+            'leader_user_id': me.leader_id,
+            'Datetime': me.datetime
+        } for me in meetings]
+    }
     return jsonify(ret), 200
 
 
 @bp.route('/meetings/invited', methods=['GET'])
 @jwt_required
 def get_invited_list():
-    page = request.args.get('page', 1, type=int)
     user_id = request.args.get('user_id', type=int)
     cur_user = Users.query.get(user_id)
     if not cur_user:
         return jsonify({"msg": "Failed to find this user", "data": ""}), 401
-    meetings = cur_user.invited_meetings().paginate(page, app.config['PER_PAGE'], False)
-    ret = {'msg': 'Success',
-           'data': [{'meeting_id': me.id,
-                     'meeting_name': me.name,
-                     'leader_user_id': me.leader_id,
-                     'Datetime': me.datetime} for me in meetings.iter_pages()]}
+    meetings = cur_user.invited_meetings().all()
+    ret = {
+        'msg':
+            'Success',
+        'data': [{
+            'meeting_id': me.id,
+            'meeting_name': me.name,
+            'leader_user_id': me.leader_id,
+            'Datetime': me.datetime
+        } for me in meetings]
+    }
     return jsonify(ret), 200
-
 
 
 @bp.route('/meetings/past', methods=['GET'])
 @jwt_required
 def get_past_list():
-    page = request.args.get('page', 1, type=int)
     user_id = request.args.get('user_id', type=int)
     cur_user = Users.query.get(user_id)
     if not cur_user:
         return jsonify({"msg": "Failed to find this user", "data": ""}), 401
-    meetings = cur_user.past_meetings().paginate(page, app.config['PER_PAGE'], False)
-    ret = {'msg': 'Success',
-           'data': [{'meeting_id': me.id,
-                     'meeting_name': me.name,
-                     'leader_user_id': me.leader_id,
-                     'Datetime': me.datetime} for me in meetings.iter_pages()]}
+    meetings = cur_user.past_meetings().all()
+    ret = {
+        'msg':
+            'Success',
+        'data': [{
+            'meeting_id': me.id,
+            'meeting_name': me.name,
+            'leader_user_id': me.leader_id,
+            'Datetime': me.datetime
+        } for me in meetings]
+    }
     return jsonify(ret), 200
 
 
 @bp.route('/meetings/all_past', methods=['GET'])
 @jwt_required
 def get_allpast_list():
-    page = request.args.get('page', 1, type=int)
     meetings = Meetings.query.filter(Meetings.datetime < datetime.utcnow())\
-            .order_by(Meetings.datetime.desc()).paginate(page, app.config['PER_PAGE'], False)
-    ret = {'msg': 'Success',
-           'data': [{'meeting_id': me.id,
-                     'meeting_name': me.name,
-                     'leader_user_id': me.leader_id,
-                     'Datetime': me.datetime} for me in meetings.iter_pages()]}
+            .order_by(Meetings.datetime.desc()).all()
+    ret = {
+        'msg':
+            'Success',
+        'data': [{
+            'meeting_id': me.id,
+            'meeting_name': me.name,
+            'leader_user_id': me.leader_id,
+            'Datetime': me.datetime
+        } for me in meetings]
+    }
     return jsonify(ret), 200
